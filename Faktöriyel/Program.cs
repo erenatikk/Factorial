1ce0633046fc34ae2c6a7cbaf3992499468e77f1
erenatikk <erenatik7@outlook.com>
2021-07-28 22:47:06 +0300

    Add project files.

---
+++ Faktöriyel/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Faktöriyel
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a number");
+            int i;
+            int number = int.Parse(Console.ReadLine());
+            int result = 1;
+            for (i = 1; i <= number ;i++)
+            {
+                result *= i;
+                
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
